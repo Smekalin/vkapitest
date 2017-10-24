940c797f8771de02af4f5d3e3b0e05755aabfdf8
Sergey Smekalin <ssa-93@mail.ru>
2017-10-24 20:18:20 +0400

    fix bug addEventListener. Auth func was moved

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,29 @@
-let userId = 1;
+VK.init({
+  apiId: 6231710
+});
+VK.Auth.getLoginStatus(function (response) {
+  if (response.session) {
+    showBlocksWhenAuthorized();
+    showFriends();
+  } else {
+    showBlocksWhenUnauthorized();
+  }
+})
+
+function showBlocksWhenUnauthorized() {
+  const loginButton = document.getElementById("login-button");
+  loginButton.style.display = "block";
+  const friendsBlock = document.getElementById("friends");
+  friendsBlock.style.display = "none";
+}
+
+function showBlocksWhenAuthorized() {
+  const loginButton = document.getElementById("login-button");
+  loginButton.style.display = "none";
+  const friendsBlock = document.getElementById("friends");
+  friendsBlock.style.display = "block";
+}
+
 const button = document.getElementById("login-button");
 button.addEventListener("click", _ => {
   VK.Auth.login(function(response) {
